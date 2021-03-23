984c61c32dab2a92376190631832087f08e29df0
marina <marina.korotenko@gmail.com>
2021-03-23 19:02:13 +0200

    commit1

---
+++ src/org/marina/it_cluster/App.java
@@ -0,0 +1,9 @@
+package org.marina.it_cluster;
+
+public class App {
+    public static void main(String[] args){
+        System.out.println("BlaBla1");
+        System.out.println("BlaBla2");
+        System.out.println("BlaBla3");
+    }
+}
